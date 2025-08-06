9cab0c16b49cc89be9fd384b32b305ea448965f4
Brincepineda <brinc.yron@gmail.com>
2025-08-06 14:55:26 +0800

    Added Bootstrap template and Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('welcome_message');
+        // Load the view content (your actual page content)
+        $content = view('home'); // we'll create this view next
+
+        // Load the template and inject the content
+        return view('template', ['content' => $content]);
     }
 }
